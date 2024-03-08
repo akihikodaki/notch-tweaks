06bf8ddafe7501524a23bf9b5d1ff69bae256f9a
Akihiko Odaki <akihiko.odaki@gmail.com>
2024-03-08 17:19:12 +0900

    Fix session mode transition

--- extension.js
+++ extension.js
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: 0BSD
 
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-import {panel} from 'resource:///org/gnome/shell/ui/main.js';
+import {panel, sessionMode} from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class extends Extension {
     enable() {
-        const { dateMenu } = panel.statusArea;
-        let children = panel._centerBox.get_children();
+        const {emit} = sessionMode;
 
         this._oldHeight = panel.height;
 
@@ -14,24 +13,23 @@ export default class extends Extension {
             panel.height = 58;
         }
 
-        if (children.includes(dateMenu.container)) {
-            panel._centerBox.remove_actor(dateMenu.container);
+        sessionMode.emit = function() {
+            this.panel = {
+                ...this.panel,
+                left: this.panel.left.concat(this.panel.center),
+                center: []
+            };
 
-            children = panel._leftBox.get_children();
-            panel._leftBox.insert_child_at_index(dateMenu.container,
-                                                 children.length - 1);
-        }
+            emit.apply(this, arguments);
+        };
+
+        sessionMode.emit('updated');
     }
 
     disable() {
-        const { dateMenu } = panel.statusArea;
-        const children = panel._leftBox.get_children();
-
         panel.height = this._oldHeight;
 
-        if (children.includes(dateMenu.container)) {
-            panel._leftBox.remove_actor(dateMenu.container);
-            panel._centerBox.add_actor(dateMenu.container);
-        }
+        delete sessionMode.emit;
+        sessionMode._sync();
     }
 }
